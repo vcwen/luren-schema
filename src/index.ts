95f0394e47b00b24473dfe1ffc431684ff11926b
vincent.wen <wenwei1202@gmail.com>
2019-05-09 17:37:24 +0800

    fix(index.ts): remove export *

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export * from './types'
-export * from './decorators/Prop'
-export * from './decorators/Schema'
-export * from './lib/IncomingFile'
+export { IJsSchema, IJsonOptions, ITypeOptions } from './types'
+export { IPropOptions, Prop, PropMetadata } from './decorators/Prop'
+export { ISchemaOptions, Schema, SchemaMetadata } from './decorators/Schema'
+export { IncomingFile } from './lib/IncomingFile'
 export {
   defineSchema,
   addType,
@@ -12,6 +12,6 @@ export {
   normalizeSimpleSchema,
   jsSchemaToJsonSchema
 } from './lib/utils'
-export * from './lib/JsonDataType'
-export * from './lib/DataType'
-export * from './constants/MetadataKey'
+export { jsonDataType, JsonDataType } from './lib/JsonDataType'
+export { dataType, DataType } from './lib/DataType'
+export { MetadataKey } from './constants/MetadataKey'
