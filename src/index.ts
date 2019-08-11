e028de2c8318af290a944013affa7100693659f9
Vincent Wen <wenwei1202@gmail.com>
2019-08-11 22:31:03 +0800

    refactor(exports):

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,8 @@
 export { IJsSchema, IJsonSchema, SimpleType } from './types'
 export { IPropOptions, Prop, PropMetadata } from './decorators/Prop'
 export { ISchemaOptions, Schema, SchemaMetadata } from './decorators/Schema'
-export {
-  defineSchema,
-  validate,
-  serialize,
-  deserialize,
-  normalizeSimpleSchema,
-  convertSimpleSchemaToJsSchema,
-  toJsonSchema,
-  getJsSchema
-} from './lib/utils'
+import * as utils from './lib/utils'
+export { utils }
 export { DataTypes } from './lib/DataTypes'
 export { MetadataKey } from './constants/MetadataKey'
 export * from './lib/JsType'
