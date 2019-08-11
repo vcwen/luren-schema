b89bdc08ce03f7ee757321db645a41e97caec9c1
Vincent Wen <wenwei1202@gmail.com>
2019-08-11 22:30:45 +0800

    refactor(constants): using string metadata key instead of symbol key

--- src/constants/MetadataKey.ts
+++ src/constants/MetadataKey.ts
@@ -1,7 +1,13 @@
+const METADATA_KEY_PREFIX = 'luren-schema:'
+
+const key = (metadataKey: string) => {
+  return METADATA_KEY_PREFIX + metadataKey
+}
 export const MetadataKey = {
-  SCHEMA: Symbol('SCHEMA'),
-  PROPS: Symbol('PROPS'),
-  SIMPLE_SCHEMA_RESOLVER: Symbol('SIMPLE_SCHEMA_RESOLVER')
+  SCHEMA: key('SCHEMA'),
+  PROPS: key('PROPS'),
+  SIMPLE_SCHEMA_RESOLVER: key('SIMPLE_SCHEMA_RESOLVER'),
+  JSON_SCHEMA: key('JSON_SCHEMA')
 }
 
 export default MetadataKey
--- src/constants/UtilMetadataKey.ts
+++
@@ -1,5 +0,0 @@
-export const UtilMetadataKey = {
-  JSON_SCHEMA: Symbol('JSON_SCHEMA')
-}
-
-export default UtilMetadataKey
